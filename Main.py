4abec7f322273e9e71477e6edc1ac0f76631412f
Samantha <asdrubalsamtha@gmail.com>
2025-10-02 13:12:36 +0000

    module 1

---
+++ Main.py
@@ -0,0 +1,46 @@
+# # age = 24
+# # height = 1.73
+# # name = "Samantha"
+# # is_student = True
+# # print(is_student)
+
+
+# # Exercice 2
+# # TODO: Create two variables 'a' and 'b' with numeric values
+
+# # TODO: Perform addition, subtraction, multiplication, and division of a and b
+# # Print the results of each operation
+
+# # TODO: Calculate the remainder of a divided by b using the modulo operator
+
+# # TODO: Calculate a to the power of b using the exponentiation operator
+# a = 23
+# b = 36
+# """
+# print(a - b)
+# print(b + a)
+# print(b / a) """
+# print(a % b)
+
+# Exercice 3
+first_name = "Samantha"
+last_name = "ASDRUBAL"
+# print(first_name + last_name)
+# print(first_name.upper() + " " + last_name.upper())
+# print(len(first_name) + len(last_name))
+# mot_multiple = "Bonjour Hello Hola bonchour"
+# word = mot_multiple.split()
+# print(word)
+
+# Exercice 4
+# Phrase = "123"
+# Phrase_Interger = int(Phrase)
+# print(Phrase_Interger)
+# print(type(Phrase_Interger))
+
+# Exercice 5
+# is_student = True
+# has_driver_license = False
+a = 301
+b = 302
+print(a != b)
